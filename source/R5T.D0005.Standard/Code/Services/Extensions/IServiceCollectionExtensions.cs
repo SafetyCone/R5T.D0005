20f7fffdaeec5f029c2425c0f37ef12f9434f0da
MinexAutomation <minex.automation@gmail.com>
2020-05-15 01:52:13 -0700

    Added the R5T.D0005.Standard library.

---
+++ source/R5T.D0005.Standard/Code/Services/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using R5T.Dacia;
+
+using R5T.D0005.Default;
+
+
+namespace R5T.D0005.Standard
+{
+    public static class IServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Adds the <see cref="IProgramNameProvider"/> service.
+        /// </summary>
+        public static IServiceCollection AddProgramNameProvider(this IServiceCollection services)
+        {
+            services.AddDefaultProgramNameProvider();
+
+            return services;
+        }
+
+        /// <summary>
+        /// Adds the <see cref="IProgramNameProvider"/> service.
+        /// </summary>
+        public static ServiceAction<IProgramNameProvider> AddProgramNameProviderAction(this IServiceCollection services)
+        {
+            var serviceAction = ServiceAction<IProgramNameProvider>.New(() => services.AddProgramNameProvider());
+            return serviceAction;
+        }
+    }
+}
